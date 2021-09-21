bb110acf03518a5958b364bf3c53e4998f9fb041
tubobfav <91153189+tubobfav@users.noreply.github.com>
2021-09-21 20:20:31 +0200

    Add files via upload

---
+++ requeriments.py
@@ -0,0 +1,30 @@
+from colors import red, green, blue, white, yellow
+
+import os, time
+
+red()
+print("1 -->> Arch")
+print("2 -->> Debian")
+print("")
+red()
+x = input("-->> ")
+
+if x == "1":
+    ed()
+    print("Downloading...")
+    os.system("sudo pacman -S git bash wpscan php dnsmasq hostap --noconfirm")
+    time.sleep(1)
+    red()
+    print("Done!!")
+    time.sleep(2)
+    exit()
+
+if x == "2":
+    red()
+    print("Downloading...")
+    os.system("sudo apt install git bash wpscan php dnsmasq hostapd -y")
+    time.sleep(1)
+    red()
+    print("Hecho!!")
+    time.sleep(2)
+    exit()
