aef75cc245be3ba91c90053a7599329816f0a023
tubobfav <91153189+tubobfav@users.noreply.github.com>
2021-09-21 21:36:11 +0200

    Add files via upload

---
+++ pishtool.py
@@ -0,0 +1,115 @@
+from colors import green, red, blue, yellow, purple, white
+
+import os, time
+
+#   <--Menu-->
+
+red()
+
+banner = """
+
+
+:::::::::   ::::::::  :::::::::  
+:+:    :+: :+:    :+: :+:    :+: 
++:+    +:+ +:+    +:+ +:+    +:+ 
++#++:++#+  +#+    +:+ +#++:++#+  
++#+    +#+ +#+    +#+ +#+    +#+ 
+#+#    #+# #+#    #+# #+#    #+# 
+#########   ########  #########                                                 
+"""
+
+def decoracion():
+    red()
+    # hola
+    print("              |                    1 -->> Campish")
+    print("              |                    2 -->> Phishing")                                      
+    print("              |                    99 -->> Exit")    
+    option = input("              +-> ")
+
+    if option == "1":
+        campish()
+
+    if option == "2":
+        phishing()
+
+    if option == "99":
+        os.system("clear")
+        exit()
+
+#   <--Return to Menu-->
+def start_menu():
+    os.system("clear")
+    red()
+    print(banner)
+    purple()
+    decoracion()
+
+
+#<--campish-->
+def campish():
+    os.system("clear")
+    red()
+    print(banner)
+    red()
+    print("              |                    1 -->> Download Tool")
+    print("              |                    2 -->> Execute tool")
+    print("              |                    3 -->> Exit")
+    x = input("              ↳ ")
+
+    print("")
+    if x == "1":
+        yellow()
+        print("")
+        print("Descargando pa...")
+        os.system("git clone https://github.com/hangetzzu/saycheese.git")
+        red()
+        print("Downloaded!!")
+        time.sleep(2)
+        while True:
+            campish()
+
+    if x == "2":
+        print("")
+        os.system("mv saycheese/* .")
+        os.system("mv saycheese/.sites .")
+        os.system("bash saycheese.sh")
+
+    if x == "3":
+        start_menu()
+
+#<--zphisher-->
+def phishing():
+    os.system("clear")
+
+    red()
+    print(banner)
+    purple()
+    print("              |                    1 -->> Download Tool")
+    print("              |                    2 -->> Execute tool")
+    print("              |                    3 -->> Exit")
+    x = input("              ↳ ")
+
+    print("")
+    if x == "1":
+        
+        yellow()
+        os.system("git clone https://github.com/htr-tech/zphisher")
+        print("")
+        red()
+        print("Fiumba ya esta!!")
+        time.sleep(1)
+        while True:
+            phishing()
+
+    if x == "2":
+        print("")
+        os.system("mv zphisher/* .")
+        os.system("mv zphisher/.sites .")
+        os.system("bash zphisher.sh")
+
+    if x == "3":
+        start_menu()
+
+
+#    <---Start the tool-->
+start_menu()
